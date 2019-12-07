68bf7cd8805d395027a33084f285cdf460050bce
Roman <sydoruk89@gmail.com>
2019-12-07 12:10:38 -0800

    form edition

--- public/js/menu.js
+++ public/js/menu.js
@@ -7,21 +7,3 @@ $(document).ready(function(){
   });
 });
 
-const validateForm = () => {
-  let $a = $('#startdate').val().split('-');
-  let $year1 = parseInt($a[0]);
-  let $month1 = parseInt($a[1]);
-  let $day1 = parseInt($a[2]);
-  let $b = $('#enddate').val().split('-');
-  let $year2 = parseInt($b[0]);
-  let $month2 = parseInt($b[1]);
-  let $day2 = parseInt($b[2]);
-  let $sub = $day2 - $day1;
-  if (!($month1 === $month2 && $year1 === $year2 && $sub < 7)) {
-    alert ('Please enter dates within 7 days');
-    return false;
-  }
-};
-
-
-$('#new-search').on('submit', validateForm);
