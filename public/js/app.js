e95a81d9158ca23ce1c860623003498c04b1752f
Roman <sydoruk89@gmail.com>
2019-12-07 04:01:10 -0800

    form validation & style

--- public/js/app.js
+++ public/js/app.js
@@ -12,5 +12,23 @@ function openForm(event) {
   }
 }
 
-
 $asteroidDiv.on('click', openForm);
+
+const validateForm = () => {
+  let $a = $('#startdate').val().split('-');
+  let $year1 = parseInt($a[0]);
+  let $month1 = parseInt($a[1]);
+  let $day1 = parseInt($a[2]);
+  let $b = $('#enddate').val().split('-');
+  let $year2 = parseInt($b[0]);
+  let $month2 = parseInt($b[1]);
+  let $day2 = parseInt($b[2]);
+  let $sub = $day2 - $day1;
+  if (!($month1 === $month2 && $year1 === $year2 && $sub < 7)) {
+    alert ('Please enter dates within 7 days');
+    return false;
+  }
+};
+
+
+$('#new-search').on('submit', validateForm);
