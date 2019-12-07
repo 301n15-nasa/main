b40c9860ab3e0a245aa1a782aa10720ebcc97cec
Noah Rhodes <noahmrhodes@gmail.com>
2019-12-07 12:17:16 -0800

    modified sql

--- data/schema.sql
+++ data/schema.sql
@@ -2,12 +2,16 @@ DROP TABLE IF EXISTS asteroid;
 
 CREATE TABLE asteroid (
     id SERIAL PRIMARY KEY,
+    name text,
     date text,
     link text,
     meters text,
     feet text,
     hazardous boolean,
     kmh text,
-    mph text
+    mph text,
+    miss_au text,
+    miss_km text,
+    miss_mi text
 );
-INSERT INTO asteroid (date, link, meters, feet, hazardous, kmh, mph) VALUES ('qwe', '12', '123', 'asd', true, 'dsa', 'we');
+INSERT INTO asteroid (name, date, link, meters, feet, hazardous, kmh, mph) VALUES ('asteroid_name','asteroid_date', 'asteroid_link', 'asteroid_meters', 'asteroid_feet', true, 'asteroid_kmh', 'asteroid_mi','miss_au','miss_km','miss_mi');
